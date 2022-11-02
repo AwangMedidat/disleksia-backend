5aca216c92d30425677ae706539c12a26df77ecd
Awang <awangmedidat98@gmail.com>
2022-11-02 14:59:29 +0700

    change-app

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
-require("../src/db/connection");
+require("./src/db/connection");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
 // const userModel = require("../src/models/userModel");
-const router = require("./routers/index");
+const router = require("./src/routers/index");
 const cors = require('cors')
 
 app.use(express.json());
 app.use(cors())
 app.use(router)
+
+router.get("/", async (req, res) => {
+  try {
+    res.status(200).json("Sukses");
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 app.listen(port, () => {
   console.log(`connection is live at port ${port}`);
